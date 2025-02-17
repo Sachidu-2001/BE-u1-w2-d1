bfc084c52640c3627ce6744d1e4c8b5e1698f78e
Sachidu-2001 <fsachi0001@gmail.com>
2025-02-17 17:03:32 +0100

    commit

---
+++ ConsoleAppW2D1/ConsoleAppW2D1/Moduls/Cibo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppW2D1.Moduls
+{
+    internal class Cibo
+    {
+        public decimal Coca_Cola { get; set; } = 2.50m;
+        public decimal Insalata_di_Pollo { get; set; } = 5.20m;
+        public decimal Pizza_Margherita { get; set; } = 10.00m;
+        public decimal Pizza_4_Formaggi { get; set; } = 12.50m;
+        public decimal Pz_Patatine_Fritte { get; set; } = 3.50m;
+        public decimal Insalata_di_Riso { get; set; } = 8.00m;
+        public decimal Frutta_di_Stagione { get; set; } = 5.00m;
+        public decimal Pizza_Fritta { get; set; } = 5.00m;
+        public decimal Piadina_Vegetariana { get; set; } = 6.00m;
+        public decimal Panino_Hamburger { get; set; } = 7.90m;
+
+
+
+    }
+}
