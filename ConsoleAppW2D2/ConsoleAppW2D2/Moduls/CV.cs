e158cbc4d4c44f214bd2a0a378018983b547243b
Sachidu-2001 <fsachi0001@gmail.com>
2025-02-18 16:59:13 +0100

    commit

---
+++ ConsoleAppW2D2/ConsoleAppW2D2/Moduls/CV.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppW2D2.Moduls
+{
+    public class CV
+    {
+        public class InformazioniPersonali 
+        {
+            public string Nome { get; set; } = "";
+            public string Cognome { get; set; } = "";
+            public string Telefono { get; set; } = "";
+            public string Email { get; set; } = "";
+
+        }
+        public class StudiEffettuati
+        {
+            public string Qualifica { get; set; }
+            public string Istituto { get; set; }
+            public string Tipo { get; set; }
+            public DateTime Dal { get; set; }
+            public DateTime Al { get; set; }
+        }
+        public class Impieghi
+        {
+            public class Esperienza
+            {
+                public string Azienda { get; set; }
+                public string JobTitle { get; set; }
+                public DateTime Dal { get; set; }
+                public DateTime Al { get; set; }
+                public string Descrizione { get; set; }
+                public string Compiti { get; set; }
+            }
+        }
+
+
+
+    }
+}
---
+++ ConsoleAppW2D2/ConsoleAppW2D2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppW2D2.Moduls;
+
+Console.WriteLine("Curriculum Vitae Layout");
+
+
+var cv = new CV();
+
+
+Console.WriteLine("Inserisci Nome");
+
+
+
+
+
+
+
+
+
